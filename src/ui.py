faa0143ff868d3fe391a1e16c2b9b1d1ca89b1eb
Katarina Strenkova <kstrenko@redhat.com>
2025-10-20 16:19:04 +0200

    UI file refactor and adding text editor
    The variable naming in the ui.py file were a bit generic and
    unreadable, so I have done a small naming refactor.
    
    Additionally, there is a new feature added - Text Editor. Now,
    the user is able to open text editor for the latex text and click
    on the save & return button to update the changes in the addon.

--- src/ui.py
+++ src/ui.py
@@ -6,21 +6,14 @@
 import bpy
 import os
 
-from bpy.props import (StringProperty,
-                       BoolProperty,
-                       EnumProperty,
-                       FloatProperty,
-                       FloatVectorProperty,
-                       PointerProperty,
-                       )
-
-from mathutils import Vector  # vertices
-
 # functions from analyser
 from .lexical_analyser import LexicalAnalyser
 from .syntax_analyser import SyntaxAnalyser
 
+TMP_TEXT_EDIT = "tmp_latex_text_edit"
 
+
+# function gets all of the loaded fonts
 def get_loaded_fonts(self, context):
     items = []
     for font in bpy.data.fonts:
@@ -29,20 +22,20 @@ def get_loaded_fonts(self, context):
 
 
 # custom properties
-class Custom_PT(bpy.types.PropertyGroup):
-
-    latex_text: StringProperty(
-        name="Latex text",
+class LATEX_PG_Properties(bpy.types.PropertyGroup):
+    latex_text: bpy.props.StringProperty(
+        name="Text",
+        description="Input text in Latex formatting",
         default=""
     ) # type: ignore
 
-    show_font: BoolProperty(
+    show_font: bpy.props.BoolProperty(
         name="",
         description="Choose custom fonts for your text",
         default=False
     ) # type: ignore
 
-    font_path: StringProperty(
+    font_path: bpy.props.StringProperty(
         name = "",
         description="Specify a path to a font you want to get loaded",
         default="",
@@ -50,47 +43,47 @@ class Custom_PT(bpy.types.PropertyGroup):
         subtype='FILE_PATH'
     ) # type: ignore
 
-    base_font: EnumProperty(
+    base_font: bpy.props.EnumProperty(
         name = "Base",
         description="Choose a font for basic text",
         items=get_loaded_fonts
     ) # type: ignore
 
-    bold_font: EnumProperty(
+    bold_font: bpy.props.EnumProperty(
         name = "Bold",
         description="Choose a font for bold text",
         items=get_loaded_fonts
     ) # type: ignore
 
-    italic_font: EnumProperty(
+    italic_font: bpy.props.EnumProperty(
         name = "Italic",
         description="Choose a font for italic text",
         items=get_loaded_fonts
     ) # type: ignore
 
-    text_scale: FloatProperty(
+    text_scale: bpy.props.FloatProperty(
         name="Scale:",
         default=1.0,
         min=0.01
     ) # type: ignore
 
-    text_thickness: FloatProperty(
+    text_thickness: bpy.props.FloatProperty(
         name="Thickness:",
         default=0.0,
         min=0.0
     ) # type: ignore
 
-    text_location: FloatVectorProperty(
+    text_location: bpy.props.FloatVectorProperty(
         name="Location",
         subtype='XYZ'
     ) # type: ignore
 
-    text_rotation: FloatVectorProperty(
+    text_rotation: bpy.props.FloatVectorProperty(
         name="Rotation",
         subtype='EULER'
     ) # type: ignore
 
-    one_object: BoolProperty(
+    one_object: bpy.props.BoolProperty(
         name="Generate as one object",
         default=False
     ) # type: ignore
@@ -107,48 +100,112 @@ class OBJECT_PT_ME(bpy.types.Panel):
     # drawing main panel
     def draw(self, context):
         layout = self.layout
-        scene = context.scene
-        cus_pt = scene.custom_prop
+        props = context.scene.custom_prop
 
-        layout.prop(cus_pt, "latex_text")
+        row = layout.row(align=True)
+        row.prop(props, "latex_text")
+        row.operator("text.edit_text", text="", icon='TEXT')
 
         row = layout.row()
         row.label(text="Custom Fonts")
-        row.prop(cus_pt, "show_font", emboss=False,
-                 icon="TRIA_DOWN" if cus_pt.show_font else "TRIA_RIGHT")
+        row.prop(props, "show_font", emboss=False,
+                icon="TRIA_DOWN" if props.show_font else "TRIA_RIGHT")
 
         # collapse font area
-        if cus_pt.show_font:
+        if props.show_font:
             box = layout.box()
-            box.prop(cus_pt, "font_path")
-            box.operator("wm.loadfont")
+            box.prop(props, "font_path")
+            box.operator("wm.load_font")
 
             box2 = layout.box()
-            box2.prop(cus_pt, "base_font")
-            box2.prop(cus_pt, "bold_font")
-            box2.prop(cus_pt, "italic_font")
+            box2.prop(props, "base_font")
+            box2.prop(props, "bold_font")
+            box2.prop(props, "italic_font")
 
-        layout.prop(cus_pt, "text_scale")
-        layout.prop(cus_pt, "text_thickness")
+        layout.prop(props, "text_scale")
+        layout.prop(props, "text_thickness")
 
         row = layout.row(align=True)
 
         col = row.column()
-        col.prop(cus_pt,'text_location')
+        col.prop(props,'text_location')
 
         col2 = row.column()
-        col2.prop(cus_pt,'text_rotation')
+        col2.prop(props,'text_rotation')
 
-        layout.prop(cus_pt, "one_object")
+        layout.prop(props, "one_object")
 
         row2 = layout.row(align=True)
-        row2.operator("wm.addtextop")
+        row2.operator("wm.add_text")
+
+
+# text editor helper panel
+class TEXT_PT_LatexEditor(bpy.types.Panel):
+    bl_label = "Save Latex Text"
+    bl_idname = "TEXT_PT_LatexEditor"
+    bl_space_type = 'TEXT_EDITOR'
+    bl_region_type = 'UI'
+    bl_category = 'Text'
+
+    @classmethod
+    def poll(cls, context):
+        # show panel only when editing latex text
+        return TMP_TEXT_EDIT in bpy.data.texts
+
+    def draw(self, context):
+        layout = self.layout
+        layout.operator("text.save_and_return", icon='FILE_TICK')
+
+
+# edit text
+class TEXT_OT_EditText(bpy.types.Operator):
+    """Open text editor for Latex text"""
+    bl_idname = "text.edit_text"
+    bl_label = "Edit Text"
+
+    def execute(self, context):
+        # create a new text block
+        if TMP_TEXT_EDIT in bpy.data.texts:
+            bpy.data.texts.remove(bpy.data.texts[TMP_TEXT_EDIT])
+        text = bpy.data.texts.new(name=TMP_TEXT_EDIT)
+
+        # load text from the text field
+        props = context.scene.custom_prop
+        text.write(props.latex_text)
+
+        # switch to a text editor
+        context.area.type = 'TEXT_EDITOR'
+        context.area.spaces.active.text = text
+
+        return {'FINISHED'}
+
+
+# save and return to the 3D view
+class TEXT_OT_SaveAndReturn(bpy.types.Operator):
+    """Save edited Latex text and return to the 3D view"""
+    bl_idname = "text.save_and_return"
+    bl_label = "Save & Return"
+
+    def execute(self, context):
+        # get the text block
+        text = bpy.data.texts.get(TMP_TEXT_EDIT)
+        if text:
+            props = context.scene.custom_prop
+            props.latex_text = text.as_string()  # save the text
+            bpy.data.texts.remove(text)          # remove text block
+
+        # return to the 3D view
+        context.area.type = 'VIEW_3D'
+
+        self.report({'INFO'}, "Latex text has been updated")
+        return {'FINISHED'}
 
 
 # load font
 class WM_OT_LoadFont(bpy.types.Operator):
+    """Load a font"""
     bl_label = "Load font"
-    bl_idname = "wm.loadfont"
+    bl_idname = "wm.load_font"
 
     def execute(self, context):
         props = context.scene.custom_prop
@@ -174,15 +231,16 @@ class WM_OT_LoadFont(bpy.types.Operator):
 
 # add text
 class WM_OT_AddText(bpy.types.Operator):
+    """Add text in Latex formatting to the 3D view"""
     bl_label = "Add Text"
-    bl_idname = "wm.addtextop"
+    bl_idname = "wm.add_text"
 
     def execute(self, context):
-        cus_pt = context.scene.custom_prop
+        props = context.scene.custom_prop
 
         # create lexical analyser and main syntax analyser
-        lex = LexicalAnalyser(cus_pt.latex_text, 0)
-        syntax = SyntaxAnalyser(lex, context, cus_pt)
+        lex = LexicalAnalyser(props.latex_text, 0)
+        syntax = SyntaxAnalyser(lex, context, props)
 
         # parse latex text
         if not syntax.parse():
@@ -192,8 +250,8 @@ class WM_OT_AddText(bpy.types.Operator):
         # all objects in latex text
         all_obj = context.selected_objects
 
-        if len(all_obj) > 1 and cus_pt.one_object:
-            generate_one_object(context, cus_pt)
+        if len(all_obj) > 1 and props.one_object:
+            generate_one_object(context, props)
         else:
             # add empty object
             bpy.ops.object.empty_add(type='PLAIN_AXES', location=(0,0,0))
@@ -203,35 +261,27 @@ class WM_OT_AddText(bpy.types.Operator):
                 # set thickness
                 if obj.type == 'FONT':
                     # for text change extrude parameter
-                    obj.data.extrude = cus_pt.text_thickness / 2.0
+                    obj.data.extrude = props.text_thickness / 2.0
                 else:
                     # for other objects apply solidify modifier
                     solidify_mod = obj.modifiers.new(name="Solidify", type='SOLIDIFY')
-                    solidify_mod.thickness = cus_pt.text_thickness
+                    solidify_mod.thickness = props.text_thickness
                     solidify_mod.offset = 0.0
 
                 # set empty object as parent
                 obj.parent = empty_obj
 
-            empty_obj.location = cus_pt.text_location                     # move empty object
-            empty_obj.rotation_euler = cus_pt.text_rotation               # rotate empty object
+            empty_obj.location = props.text_location                      # move empty object
+            empty_obj.rotation_euler = props.text_rotation                # rotate empty object
             bpy.ops.object.transform_apply(location=True, rotation=True)  # apply transformation
             bpy.ops.object.delete()                                       # delete empty object
 
+        self.report({'INFO'}, "Latex text was generated successfully")
         return {'FINISHED'}
 
 
-# enumeration of all my classes
-all_classes = [
-    Custom_PT,
-    OBJECT_PT_ME,
-    WM_OT_LoadFont,
-    WM_OT_AddText
-]
-
-
 # function generates the final latex text as one object
-def generate_one_object(context, cus_pt):
+def generate_one_object(context, props):
     all_obj = context.selected_objects
     all_converted_obj = []
 
@@ -256,13 +306,13 @@ def generate_one_object(context, cus_pt):
 
     final_obj = context.active_object
     final_obj.name = "Latex Text"
-    final_obj.location = cus_pt.text_location                     # move object
-    final_obj.rotation_euler = cus_pt.text_rotation               # rotate object
+    final_obj.location = props.text_location                      # move object
+    final_obj.rotation_euler = props.text_rotation                # rotate object
     bpy.ops.object.transform_apply(location=True, rotation=True)  # apply transformation
 
     # apply solidify modifier
     solidify_mod = final_obj.modifiers.new(name="Solidify", type='SOLIDIFY')
-    solidify_mod.thickness = cus_pt.text_thickness
+    solidify_mod.thickness = props.text_thickness
     solidify_mod.offset = 0.0
 
     # delete base latex collection
@@ -273,10 +323,22 @@ def generate_one_object(context, cus_pt):
             bpy.data.collections.remove(collection)
 
 
+# enumeration of all my classes
+all_classes = [
+    LATEX_PG_Properties,
+    OBJECT_PT_ME,
+    TEXT_PT_LatexEditor,
+    TEXT_OT_EditText,
+    TEXT_OT_SaveAndReturn,
+    WM_OT_LoadFont,
+    WM_OT_AddText
+]
+
+
 def register():
     for cls in all_classes:
         bpy.utils.register_class(cls)
-    bpy.types.Scene.custom_prop = PointerProperty(type=Custom_PT)
+    bpy.types.Scene.custom_prop = bpy.props.PointerProperty(type=LATEX_PG_Properties)
 
 
 def unregister():
